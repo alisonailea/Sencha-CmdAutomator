52e4e54e9851ecd76d8a948250253faa9ccf368a
Alison Stump <alison@alisonstump.com>
2013-06-26 12:56:00 -0700

    added success comment to app_build

--- SenchaUpdate.sh
+++ SenchaUpdate.sh
@@ -34,7 +34,7 @@ elif [ "$comd_set" = "app_build" ] ; then
 	sencha_app_path="${2}"
 	cd $sencha_app_path/
 	sencha app build
-	echo "New "
+	echo "New Sencha App build complete."
 else
     echo "There has been an error. Please enter a command."
 fi
